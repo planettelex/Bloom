022969d64b85aefc32deef91054a9e29da64d46c
Rob Dixon <rob@planettelex.net>
2015-01-10 15:42:41 -0700

    Created a new Visual Studio solution per issue #1.

---
+++ Bloom.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bloom.Domain")]
+[assembly: AssemblyDescription("Domain model for the music business.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d0d578b-be20-4bb4-85fc-9db8419974c1")]
+
