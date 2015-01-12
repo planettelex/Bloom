663def179ebe4978f6a5c5814a60d07939ba1a58
Rob Dixon <rob@planettelex.net>
2015-01-12 15:33:04 -0700

    Added activity classes and diagram per issue #38.

---
+++ Bloom.Domain/Models/AlbumActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bloom.Domain.Models
+{
+    public class AlbumActivity
+    {
+        public Guid AlbumId { get; set; }
+
+        public Album Album { get; set; }
+
+        public Guid ActivityId { get; set; }
+
+        public Activity Activity { get; set; }
+    }
+}
---
+++ Bloom.Domain/Models/SongActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bloom.Domain.Models
+{
+    public class SongActivity
+    {
+        public Guid SongId { get; set; }
+
+        public Song Song { get; set; }
+
+        public Guid ActivityId { get; set; }
+
+        public Activity Activity { get; set; }
+    }
+}
