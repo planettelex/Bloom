7bedad8f37f1a3269b15d4e0761ad2fa013b2ddc
Rob Dixon <rob@planettelex.net>
2015-01-16 11:51:50 -0700

    Added shared services project per issue #63.

---
+++ Bloom.Services/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bloom.Services")]
+[assembly: AssemblyDescription("Service classes that may be reuesed by multiple applications.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5675796d-c93a-4496-b9e0-494e25738719")]
+
