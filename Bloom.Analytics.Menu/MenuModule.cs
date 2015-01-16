a1719450f029c51fa7b51685c7de7fd65a7863b8
Rob Dixon <rob@planettelex.net>
2015-01-16 16:02:11 -0700

    Implemented the analytics main menu as a Prism module per issue #68.

---
+++ Bloom.Analytics.Menu/MenuModule.cs
@@ -0,0 +1,31 @@
+﻿using Bloom.Analytics.Menu.Views;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace Bloom.Analytics.Menu
+{
+    /// <summary>
+    /// Analytics menu Prism module.
+    /// </summary>
+    [Module(ModuleName = "MenuModule")]
+    public class MenuModule : IModule
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuModule"/> class.
+        /// </summary>
+        /// <param name="regionManager">The region manager.</param>
+        public MenuModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+        private readonly IRegionManager _regionManager;
+
+        /// <summary>
+        /// Notifies the module that it has be initialized.
+        /// </summary>
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MenuRegion", typeof(MenuView));
+        }
+    }
+}
