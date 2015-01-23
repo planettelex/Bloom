308c6b448625abb694409b8857d7122bc04cf061
Rob Dixon <rob@planettelex.net>
2015-01-23 15:42:19 -0700

    Added a shared PubSub events assembly, and defined the events needed for issue #75.

---
+++ Bloom.PubSubEvents/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bloom.PubSubEvents")]
+[assembly: AssemblyDescription("Prism PubSub events for the Bloom Music Suite.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a54021bb-4c67-428e-9235-6452ad205725")]
+
---
+++ Bloom.PubSubEvents/ShellEvents.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Controls;
+using Microsoft.Practices.Prism.PubSubEvents;
+
+namespace Bloom.PubSubEvents
+{
+    /// <summary>
+    /// Add the passed user control to the shell as a new tab.
+    /// </summary>
+    public class AddTabEvent : PubSubEvent<UserControl> { }
+
+    /// <summary>
+    /// Create a new library control and pass it to the <see cref="AddTabEvent"/>.
+    /// </summary>
+    public class NewLibraryTab : PubSubEvent<object> { }
+
+    /// <summary>
+    /// Create a new artist control and pass it to the <see cref="AddTabEvent"/>.
+    /// </summary>
+    public class NewArtistTab : PubSubEvent<object> { }
+
+    /// <summary>
+    /// Create a new person control and pass it to the <see cref="AddTabEvent"/>.
+    /// </summary>
+    public class NewPersonTab : PubSubEvent<object> { }
+
+    /// <summary>
+    /// Create a new album control and pass it to the <see cref="AddTabEvent"/>.
+    /// </summary>
+    public class NewAlbumTab : PubSubEvent<object> { }
+
+    /// <summary>
+    /// Create a new song control and pass it to the <see cref="AddTabEvent"/>.
+    /// </summary>
+    public class NewSongTab : PubSubEvent<object> { }
+
+    /// <summary>
+    /// Create a new playlist control and pass it to the <see cref="AddTabEvent"/>.
+    /// </summary>
+    public class NewPlaylistTab : PubSubEvent<object> { }
+}
