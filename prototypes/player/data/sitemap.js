4777280e289c1ef7e2d80bd5e0996d27d741c2cf
Rob Dixon <rob@planettelex.net>
2015-02-12 12:02:42 -0700

    Updated player prototype with connected libraries dialog for issues #96.

---
+++ prototypes/player/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k)])]);}; 
+var b="rootNodes",c="pageName",d="Player",e="type",f="Wireframe",g="url",h="Player.html",i="children",j="Smallest",k="Smallest.html";
+return _creator();
+})();
