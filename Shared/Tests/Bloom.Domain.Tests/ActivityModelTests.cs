dc757447b0ec4d15e12fbe0cda7a875270de9be8
Rob Dixon <rob@planettelex.net>
2015-02-20 11:59:35 -0700

    Referenced nunit and made a domain test for issue #93.

---
+++ Shared/Tests/Bloom.Domain.Tests/ActivityModelTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Bloom.Domain.Models;
+using NUnit.Framework;
+
+namespace Bloom.Domain.Tests
+{
+    [TestFixture]
+    public class ActivityModelTests
+    {
+        [Test]
+        public void CreateActivityTest()
+        {
+            var activity = Activity.Create("Test");
+            Assert.AreNotEqual(activity.Id, Guid.Empty);
+            Assert.AreEqual(activity.Name, "Test");
+        }
+    }
+}
