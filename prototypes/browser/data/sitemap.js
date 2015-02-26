daa1be47722f1ec4ac0a08db1c95b83a2ba4033c
Rob Dixon <rob@planettelex.net>
2015-02-26 13:52:38 -0700

    Updating browser prototype with home tab per issue #94.

--- prototypes/browser/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u)])]);}; 
-var b="rootNodes",c="pageName",d="Library",e="type",f="Wireframe",g="url",h="Library.html",i="children",j="Artist Details",k="Artist_Details.html",l="Person Details",m="Person_Details.html",n="Album Details",o="Album_Details.html",p="Song Details",q="Song_Details.html",r="Playlist Details",s="Playlist_Details.html",t="Filterset Details",u="Filterset_Details.html";
-return _creator();
-})();
