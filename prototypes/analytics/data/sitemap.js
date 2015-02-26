1d6b75379b025817676d70a171793fd203cbaa88
Rob Dixon <rob@planettelex.net>
2015-02-26 13:51:17 -0700

    Updating analyics prototype with home tab per issue #94.

--- prototypes/analytics/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h)]);}; 
-var b="rootNodes",c="pageName",d="Analytics",e="type",f="Wireframe",g="url",h="Analytics.html";
-return _creator();
-})();
