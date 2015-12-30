d998b44662cc6e22b949d47a86d8ec7fc168029a
Rob Dixon <rob@planettelex.net>
2015-12-30 15:33:49 -0700

    Updated Browser prototype.

--- prototypes/browser/resources/scripts/messagecenter.js
+++ prototypes/browser/resources/scripts/messagecenter.js
@@ -5,19 +5,73 @@
 // sniff chrome
 var CHROME_5_LOCAL = false;
 var CHROME = false;
+var SAFARI = false;
 var WEBKIT = false;
+var OS_MAC = false;
+var IOS = false;
+
+var IE = false;
+var BROWSER_VERSION = 5000;
 (function () {
-    var chromeRegex = /Chrome\/([0-9]+).([0-9]+)/g ;
-    var chromeMatch = chromeRegex.exec(navigator.userAgent);
-    CHROME = Boolean(chromeMatch);
-    CHROME_5_LOCAL = chromeMatch &&
-                Number(chromeMatch[1]) >= 5 &&
-                location.href.indexOf('file://') >= 0;
+    if(!window.$axure) window.$axure = function() {};
+    var useragent = window.navigator.userAgent;
+
+    var edgeRegex = /Edge\/([0-9]+)/g ;
+    var edgeMatch = edgeRegex.exec(useragent);
+    $axure.browser = {isEdge : Boolean(edgeMatch)};
+
+    if(!$axure.browser.isEdge) {
+      var chromeRegex = /Chrome\/([0-9]+).([0-9]+)/g ;
+      var chromeMatch = chromeRegex.exec(useragent);
+      CHROME = Boolean(chromeMatch);
+      CHROME_5_LOCAL = chromeMatch &&
+                  Number(chromeMatch[1]) >= 5 &&
+                  location.href.indexOf('file://') >= 0;
+    }
+  
+    var safariRegex = /Safari\/([0-9]+)/g;
+    var safariMatch = safariRegex.exec(window.navigator.userAgent);
+    SAFARI = Boolean(safariMatch) && !CHROME; //because chrome also inserts safari string into user agent
 
     var webkitRegex = /WebKit\//g ;
-    WEBKIT = Boolean(webkitRegex.exec(navigator.userAgent));
-})();
+    WEBKIT = Boolean(webkitRegex.exec(window.navigator.userAgent));
 
+    var macRegex = /Mac/g ;
+    OS_MAC = Boolean(macRegex.exec(window.navigator.platform));
+
+    IOS = navigator.userAgent.match( /iPhone/i ) || navigator.userAgent.match( /iPad/i ) || navigator.userAgent.match( /iPod/i );
+
+    if($.browser) {
+        IE = $.browser.msie;
+        BROWSER_VERSION = $.browser.version;
+    }
+
+    //Used by sitemap and variables.js getLinkUrl functions so that they know
+    //whether to embed global variables in URL as query string or hash string
+    //_shouldSendVars persists the value for sitemap instead of re-checking every time
+    var _shouldSendVars;
+    var _shouldSendVarsToServer = function(url) {
+        if(typeof _shouldSendVars != 'undefined') {
+            return _shouldSendVars;
+        }
+
+        if(SAFARI || (IE && BROWSER_VERSION < 10)) {
+            var urlToCheck = typeof url != 'undefined' ? url : window.location.href;
+            var serverRegex = /http:\/\/127\.0\.0\.1:[0-9]{5}/g;
+            var serverMatch = serverRegex.exec(urlToCheck);
+            var previewRegex = /[0-9]{2}\.[0-9]{2}\.[0-9]{2}/g;
+            var previewMatch = previewRegex.exec(urlToCheck);
+            if(Boolean(serverMatch) && Boolean(previewMatch)) {
+                _shouldSendVars = true;
+                return _shouldSendVars;
+            }
+        }
+
+        _shouldSendVars = false;
+        return _shouldSendVars;
+    };
+    $axure.shouldSendVarsToServer = _shouldSendVarsToServer;
+})();
 
 (function() {
     var _topMessageCenter;
@@ -41,18 +95,20 @@ var WEBKIT = false;
     (function() {
         if (!CHROME_5_LOCAL) {
             var topAxureWindow = window;
-            while (topAxureWindow.parent && topAxureWindow.parent !== topAxureWindow
-                && topAxureWindow.parent.$axure) topAxureWindow = topAxureWindow.parent;
+            try {
+                while(topAxureWindow.parent && topAxureWindow.parent !== topAxureWindow
+                    && topAxureWindow.parent.$axure) topAxureWindow = topAxureWindow.parent;
+            } catch(e) {}
             _topMessageCenter = topAxureWindow.$axure.messageCenter;
         }
     })();
 
-    $(document).ready(function() {
+    $(window.document).ready(function() {
         if (CHROME_5_LOCAL) {
             $('body').append("<div id='axureEventReceiverDiv' style='display:none'></div>" +
                 "<div id='axureEventSenderDiv' style='display:none'></div>");
 
-		    _eventObject = document.createEvent('Event');
+		    _eventObject = window.document.createEvent('Event');
 		    _eventObject.initEvent('axureMessageSenderEvent', true, true);            
             
             $('#axureEventReceiverDiv').bind('axureMessageReceiverEvent', function () {
@@ -133,49 +189,49 @@ var WEBKIT = false;
         });
     };
 
-    _messageCenter.postMessage = function (message, data) {
-        if (!CHROME_5_LOCAL) {
+    _messageCenter.postMessage = function(message, data) {
+        if(!CHROME_5_LOCAL) {
             _topMessageCenter.dispatchMessageRecursively(message, data);
         } else {
             var request = {
-                message : message,
-                data : data
+                message: message,
+                data: data
             };
-   
-            if (_initialized) {
-                var senderDiv = document.getElementById('axureEventSenderDiv');
+
+            if(_initialized) {
+                var senderDiv = window.document.getElementById('axureEventSenderDiv');
                 var messageText = JSON.stringify(request);
-                console.log('sending event: ' + messageText);
+                //                console.log('sending event: ' + messageText);
                 senderDiv.innerText = messageText;
                 senderDiv.dispatchEvent(_eventObject);
-                console.log('event sent');
+                //                console.log('event sent');
             } else {
                 _queuedMessages[_queuedMessages.length] = request;
             }
         }
-    }
+    };
 
     _messageCenter.setState = function(key, value) {
         var data = {
-            key : key,
-            value : value
+            key: key,
+            value: value
         };
         _messageCenter.postMessage('setState', data);
-    }
+    };
 
     _messageCenter.getState = function(key) {
         return _state[key];
-    }
+    };
 
     _messageCenter.addMessageListener = function(listener) {
         _listeners[_listeners.length] = listener;
-    }
+    };
 
     _messageCenter.addStateListener = function(key, listener) {
         _stateListeners[_stateListeners.length] = {
             key: key,
             listener: listener
         };
-    }
+    };
 
 })();
