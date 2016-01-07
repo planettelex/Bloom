6d6c3bc4a8495ba380499e6e359c15269391735c
Rob Dixon <rob@planettelex.net>
2016-01-06 17:48:32 -0700

    Removing old browser prototype.

--- prototypes/browser/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l),_(c,m,e,f,g,n),_(c,o,e,f,g,p),_(c,q,e,f,g,r),_(c,s,e,f,g,t),_(c,u,e,f,g,v)]);}; 
-var b="rootNodes",c="pageName",d="Home",e="type",f="Wireframe",g="url",h="Home.html",i="Library",j="Library.html",k="Artist",l="Artist.html",m="Album",n="Album.html",o="Song",p="Song.html",q="Person",r="Person.html",s="Playlist",t="Playlist.html",u="Filterset",v="Filterset.html";
-return _creator();
-})();
